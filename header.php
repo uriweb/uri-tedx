d5d2049d6bcf7a50d7e3e7e16207f68b07372a9d
Brandon Fuller <bjcfuller@uri.edu>
2018-07-02 12:04:24 -0400

    Remove favicon and Apple touch icons

--- header.php
+++ header.php
@@ -19,13 +19,6 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <?php wp_head(); ?>
-	
-<!-- Favicons -->
-<link rel="mask-icon" href="<?php echo get_template_directory_uri() . '/images/safari-pinned-tab.svg'; ?>" color="#0047c2">
-<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/images/favicon.png'; ?>">
-<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri() . '/images/apple-touch-icon.png'; ?>">
-<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/images/apple-touch-icon-180x180.png'; ?>">
-
 
 </head>
 	
