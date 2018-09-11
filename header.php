896292c974b58cb5328e254d9697d28520bd8ce2
Brandon Fuller <bjcfuller@uri.edu>
2018-09-11 09:40:24 -0400

    Remove Team from navigation; reorganize nav

--- header.php
+++ header.php
@@ -42,11 +42,10 @@ if ( ! empty( $gtm ) ) {
 				<input type="checkbox" id="tedx-nav-toggle" role="presentation" aria-label="Open the TEDxURI site menu when browsing on mobile">
 				<label for="tedx-nav-toggle" id="tedx-nav-label"><span>Menu</span></label>
 				<ul id="tedx-nav">
-				  <li id="ln-about"><a href="https://www.uri.edu/tedx/about">About</a></li>
-				  <li id="ln-speakers"><a href="https://www.uri.edu/tedx/speakers">Speakers</a></li>
-				  <li id="ln-watch"><a href="https://www.uri.edu/tedx/watch">Watch</a></li>
-				  <li id="ln-team"><a href="https://www.uri.edu/tedx/team">Our Team</a></li>
-				  <li id="ln-uri-home"><a href="https://www.uri.edu/">uri.edu</a></li>
+					<li id="ln-watch"><a href="https://www.uri.edu/tedx/watch">Talks</a></li>
+					<li id="ln-speakers"><a href="https://www.uri.edu/tedx/speakers">Speakers</a></li>
+					<li id="ln-about"><a href="https://www.uri.edu/tedx/about">About</a></li>
+					<li id="ln-uri-home"><a href="https://www.uri.edu/">uri.edu</a></li>
 				</ul>
 			</div>
 
