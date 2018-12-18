fe73674d8f95b951ee4a48bbd664cd865d4c2e19
Brandon Fuller <bjcfuller@uri.edu>
2018-12-18 10:59:19 -0500

    Add some styles and js to control "see more" text in speaker blocks; renamed an older js file

---
+++ src/js/2019-speakers.js
@@ -0,0 +1,41 @@
+/**
+ * 2019 Speakers
+ *
+ * @package uri-tedx
+ */
+
+( function() {
+
+	window.addEventListener( 'load', initSpeakers, false );
+
+	function initSpeakers() {
+
+		var els, el, parent, i;
+
+		els = document.querySelectorAll( '.block-slider-trigger' );
+
+		for ( i = 0; i < els.length; i++ ) {
+
+			parent = els[i].parentElement;
+			els[i].addEventListener( 'click', handleSlider.bind( null, parent, els[i] ), false );
+
+		}
+
+	}
+
+	function handleSlider( parent, trigger ) {
+
+		var className = 'slider-open';
+		console.log('clicked');
+
+		if ( parent.classList.contains( className ) ) {
+			parent.classList.remove( className );
+			trigger.innerHTML = 'See More';
+		} else {
+			parent.classList.add( className );
+			trigger.innerHTML = 'See Less';
+		}
+
+	}
+
+})();
