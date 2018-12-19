3706a95b2a954709d999efedd762b7a13b167e71
Brandon Fuller <bjcfuller@uri.edu>
2018-12-19 18:31:05 -0500

    Adjust animations on speakers page

--- src/js/2019-speakers.js
+++ src/js/2019-speakers.js
@@ -10,30 +10,28 @@
 
 	function initSpeakers() {
 
-		var els, el, block, parent, i;
+		var els, el, block, i;
 
 		els = document.querySelectorAll( '.block-slider-trigger' );
 
 		for ( i = 0; i < els.length; i++ ) {
 
-			parent = els[i].parentElement;
-			block = parent.parentElement;
-			block.addEventListener( 'click', handleSlider.bind( null, parent, els[i] ), false );
+			block = els[i].parentElement.parentElement;
+			block.addEventListener( 'click', handleSlider.bind( null, block, els[i] ), false );
 
 		}
 
 	}
 
-	function handleSlider( parent, trigger ) {
+	function handleSlider( block, trigger ) {
 
 		var className = 'slider-open';
-		console.log( 'clicked' );
 
-		if ( parent.classList.contains( className ) ) {
-			parent.classList.remove( className );
+		if ( block.classList.contains( className ) ) {
+			block.classList.remove( className );
 			trigger.innerHTML = 'Read Synopsis';
 		} else {
-			parent.classList.add( className );
+			block.classList.add( className );
 			trigger.innerHTML = 'Close Synopsis';
 		}
 
