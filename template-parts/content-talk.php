4bf8eeece158accaad9cea3bcb5a2a8d7b3a17de
Brandon Fuller <bjcfuller@uri.edu>
2024-03-07 11:56:52 -0500

    Remove youtube statistics

--- template-parts/content-talk.php
+++ template-parts/content-talk.php
@@ -49,35 +49,6 @@
 						<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 					</div>
 					<div class="smi-widget share-button share-facebook"><iframe src="https://www.facebook.com/plugins/share_button.php?href=<?php echo urlencode( utf8_encode( get_permalink() ) ); ?>&layout=button&size=large&mobile_iframe=true&width=73&height=28&appId" width="73" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe></div>
-					<div class="smi-widget yt-statistics">
-					<?php
-
-					/**
-					 * Get the video data
-					 */
-					$key = 'AIzaSyC-6FV_g5x8dSIBNHW3kEh3VP8lu9qnb2c';
-					$vid = uri_tedx_get_field( 'ytid' );
-					$json = file_get_contents( 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id=' . $vid . '&key=' . $key );
-					$json_data = json_decode( $json, true );
-
-					$views = $json_data['items'][0]['statistics']['viewCount'];
-					$likes = $json_data['items'][0]['statistics']['likeCount'];
-
-					/* View Count */
-					if ( $views ) {
-						$view_label = 1 == $views ? 'view' : 'views';
-						echo '<span class="yt-stat yt-views"><strong>' . number_format( $views ) . '</strong> ' . $view_label . '</span>';
-					}
-
-					/*
-					 Like Count
-					if ($likes || $likes == 0) {
-						echo '<span class="yt-stat yt-likes" title="Likes on YouTube">' . number_format($likes) . '</span>';
-					}
-					*/
-
-					?>
-					</div>
 				</div>
 			</section>
 
